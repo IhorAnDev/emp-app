d20103477f8527612ca90cf94ab24f76f65624bb
IhorAnDev <ihorandev@gmail.com>
2023-04-10 21:04:54 +0300

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,15 @@
+import "./search-panel.css"
+
+const SearchPanel = () => {
+
+    return (
+        <div>
+            <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Search employee"/>
+        </div>
+    )
+}
+
+export default SearchPanel;
