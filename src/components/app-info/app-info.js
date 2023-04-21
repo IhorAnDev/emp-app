5cdcfb407aee3de465cfd40f3fce09f65514a087
IhorAnDev <ihorandev@gmail.com>
2023-04-22 00:31:57 +0300

    info fix

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -8,8 +8,8 @@ const AppInfo = ({data}) => {
     return (
         <div className="app-info">
             <h1>Accounting of employees in the company "Sparta"</h1>
-            <h2>Total number of employees: {totalQuantityEmp} </h2>
-            <h2>Will receive the award: {empToAward} </h2>
+            <h2>Total number of employees: {totalQuantityEmp ? totalQuantityEmp : ""} </h2>
+            <h2>Will receive the award: {empToAward ? empToAward : ""} </h2>
         </div>
     )
 }
