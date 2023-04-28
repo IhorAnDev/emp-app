83f4e8d483c4fbc16df597390d79b0839e2c75c1
IhorAnDev <ihorandev@gmail.com>
2023-04-28 23:43:56 +0300

    rewrite into hook form

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,64 +1,56 @@
 import "./employees-add-form.css";
-import {Component} from "react";
+import {Component, useState} from "react";
 
-class EmployeesAddForm extends Component {
+const EmployeesAddForm = ({addEmployee}) => {
 
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            salary: ''
+    const [name, setName] = useState("");
+    const [salary, setSalary] = useState("");
+
+    const onFillInput = (e) => {
+        const {name, value} = e.target;
+        if (name === 'name') {
+            setName(value);
+        } else if (name === 'salary') {
+            setSalary(value)
         }
     }
 
-    onFillInput = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-    onAddEmployee = (event, {addEmployee} = this.props) => {
+    const onAddEmployee = (event) => {
         event.preventDefault();
-        const {name, salary} = this.state;
         if (name.length < 3 || !salary) {
             return;
         }
         addEmployee(name, salary);
-        this.setState({name: "", salary: ""});
+        setName("");
+        setSalary("");
     }
 
 
-    render() {
-
-        const {name, salary} = this.state;
-
-
-        return (
-            <div className="app-add-form">
-                <h3>Add new employee</h3>
-                <form
-                    onSubmit={this.onAddEmployee}
-                    className="add-form d-flex">
-                    <input type="text"
-                           className="form-control new-post-label"
-                           name="name"
-                           onChange={(event) => this.onFillInput(event)}
-                           value={name}
-                           placeholder="Name employee?"/>
-                    <input type="number"
-                           className="form-control new-post-label"
-                           name="salary"
-                           onChange={(event) => this.onFillInput(event)}
-                           value={salary}
-                           placeholder="Salary in $?"/>
-                    <button type="submit"
-                            className="btn btn-outline-light">Add
-                    </button>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="app-add-form">
+            <h3>Add new employee</h3>
+            <form
+                onSubmit={onAddEmployee}
+                className="add-form d-flex">
+                <input type="text"
+                       className="form-control new-post-label"
+                       name="name"
+                       onChange={(event) => onFillInput(event)}
+                       value={name}
+                       placeholder="Name employee?"/>
+                <input type="number"
+                       className="form-control new-post-label"
+                       name="salary"
+                       onChange={(event) => onFillInput(event)}
+                       value={salary}
+                       placeholder="Salary in $?"/>
+                <button type="submit"
+                        className="btn btn-outline-light">Add
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default EmployeesAddForm;
